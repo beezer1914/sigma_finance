42acdf2beb4c67c6047b1cff0fa0249d3ba928a4
Brandon Holiday <bholi1914@outlook.com>
2025-08-27 11:45:35 -0400

    created the invite automation functionality

--- sigma_finance/utils/generate_invite.py
+++ sigma_finance/utils/generate_invite.py
@@ -1,4 +1,6 @@
 import secrets
+from datetime import datetime
 
-def generate_invite_code(length=16):
-    return secrets.token_urlsafe(length)[:length]
+def generate_invite_code(role="member", expires_in_days=7):
+    code = secrets.token_urlsafe(8)
+    return code, datetime.utcnow() + datetime.timedelta(days=expires_in_days), role
