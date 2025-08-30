895350f7f19952bd24afbb78f18fd5010f727834
Brandon Holiday <bholi1914@outlook.com>
2025-08-30 11:39:08 -0400

    stub for missing revision

---
+++ migrations/versions/5e30e0c904e6_add_created_at_to_invitecode.py
@@ -0,0 +1,12 @@
+"""stub for invite_code columns already in prod"""
+
+revision = '5e30e0c904e6'
+down_revision = 'b4bfad26f602'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
