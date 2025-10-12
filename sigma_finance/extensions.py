5464d648430ce70f5e5189c334dc75417ee2210d
Brandon Holiday <bholi1914@outlook.com>
2025-10-12 08:13:25 -0400

    Implemented security headers

--- sigma_finance/extensions.py
+++ sigma_finance/extensions.py
@@ -1,9 +1,12 @@
+# sigma_finance/extensions.py
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_talisman import Talisman  # ADD THIS
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -12,5 +15,6 @@ cache = Cache()
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",  # use Redis in production
+    storage_uri="memory://"
 )
+talisman = Talisman()  
